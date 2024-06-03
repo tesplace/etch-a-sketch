86e7c74a47c7fbb6a17d50b79a70686699e2fbb8
tesplace <161244753+tesplace@users.noreply.github.com>
2024-06-03 21:09:24 +0200

    Creates the finished js and half worked css

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const container = document.querySelector(".container");
 const btnGo = document.querySelector(".btnGo");
-
-const input = document.getElementById("input")
+const input = document.getElementById("input");
+const reset = document.querySelector(".reset")
 
 
 btnGo.addEventListener("click", () => {
     const sketchBoardSize = input.value;
 
+    // code that draws the sketchboard
 
     if (sketchBoardSize <= 100 && sketchBoardSize > 0) {
         for (let i = 0; i < sketchBoardSize; i++) {
@@ -16,18 +17,31 @@ btnGo.addEventListener("click", () => {
             for (let i = 0; i < sketchBoardSize; i++) {
                 const square = document.createElement("div");
                 square.className = "square";
+
+                square.addEventListener("mouseover", () => {
+                    square.classList.add("black");
+                });
     
                 row.appendChild(square);
             }
 
             container.appendChild(row);            
         }
+
+        btnGo.disabled = true;
     } else {
-        alert("error");
-    }     
+        alert("Please enter a number!");
+        btnGo.disabled = false;
+    };     
+
+
+
+    // reset button 
+
+    reset.addEventListener("click", ()=> {
+        container.innerHTML = " ";
+        btnGo.disabled = false;
+        input.value = "Enter a new number!"
+    });
+
 });
-
-const squareHover = document.getElementsByClassName("square")
-
-squareHover.setAttribute("style", "background: black;");
-
